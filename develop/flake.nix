15334c95657558fbbb10c6c8f345e6e58bb57a01
hugosenari <hugosenari@gmail.com>
2022-03-22 04:31:49 -0300

    chore: re index nest

--- develop/flake.nix
+++ develop/flake.nix
@@ -10,7 +10,7 @@
   inputs.src-nest-develop.flake = false;
   inputs.src-nest-develop.ref   = "refs/heads/develop";
   inputs.src-nest-develop.owner = "kedean";
-  inputs.src-nest-develop.repo  = "nest.git";
+  inputs.src-nest-develop.repo  = "nest";
   inputs.src-nest-develop.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
--- master/flake.nix
+++ master/flake.nix
@@ -10,7 +10,7 @@
   inputs.src-nest-master.flake = false;
   inputs.src-nest-master.ref   = "refs/heads/master";
   inputs.src-nest-master.owner = "kedean";
-  inputs.src-nest-master.repo  = "nest.git";
+  inputs.src-nest-master.repo  = "nest";
   inputs.src-nest-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
