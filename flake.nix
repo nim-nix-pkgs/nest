d766510913cb644c879cec50d36afa1bb3e8a934
hugosenari <hugosenari@gmail.com>
2022-05-09 20:40:28 -0300

    chore: re index nest

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."nest-develop".dir   = "develop";
-  inputs."nest-develop".owner = "nim-nix-pkgs";
-  inputs."nest-develop".ref   = "master";
-  inputs."nest-develop".repo  = "nest";
-  inputs."nest-develop".type  = "github";
-  inputs."nest-develop".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nest-develop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."nest-master".dir   = "master";
-  inputs."nest-master".owner = "nim-nix-pkgs";
-  inputs."nest-master".ref   = "master";
-  inputs."nest-master".repo  = "nest";
-  inputs."nest-master".type  = "github";
-  inputs."nest-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nest-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
